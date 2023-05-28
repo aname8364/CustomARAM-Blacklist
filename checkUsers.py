fbaa4b922c6f7cadb64c1e938a0ec7711c51fab7
aname <132615033+aname8364@users.noreply.github.com>
2023-05-29 06:46:39 +0900

    Update checkUsers.py

--- checkUsers.py
+++ checkUsers.py
@@ -14,17 +14,22 @@ connector = Connector()
 async def onPlayerJoin(connection, event):
     leagueChat  = Chat(connection)
     leagueLobby = Lobby(connection)
-    leagueSummoner = Summoner(connection)
 
     if event.uri.find("participants") != -1:
         if "gameName" in event.data:
             name = event.data["name"]
             id = event.data["summonerId"]
             badUsers = BeautifulSoup(requests.get("https://anametest.000webhostapp.com/badusers.html").text, 'html.parser')
-            badNames = [user for i, user in enumerate(badUsers.text.splitlines()) if i > 6 and len(user) > 1]
+            
+            badData = {
+                user.split("-", 1)[0].strip(): user.split("-", 1)[1].strip() if "-" in user else "?"
+                for i, user in enumerate(badUsers.text.splitlines())
+                if i > 8 and len(user) > 1
+            }
 
-            if name in badNames:
+            if name in badData:
                 print(f"[강퇴] {name}")
+                await leagueChat.SendMessage(f"checkUsers:: {name} ({id}) reason: {badData[name]}")
                 await leagueLobby.Kick(id)
 
 
